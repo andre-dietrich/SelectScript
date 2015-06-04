622198c5e82fe05aae2aec34ffa1ece867d12a9c
andre-dietrich <dietrich@ivs.cs.uni-magdeburg.de>
2015-06-04 18:16:40 +0200

    remove useless examples

--- Python/Base/SelectScript/examples/8_dames.py
+++ Python/Base/SelectScript/examples/8_dames.py
@@ -1,8 +1,9 @@
 import SelectScript
 import SelectScript.Interpreter
 
-def move(step, towers):
-    if towers == [] or towers[step[0]] == []:
+def place(row, colum, checkboard):
+    if checkboard == []: return []
+    if checkboard[row,colum] != 0:
         pass
     elif towers[step[1]] == []:
         towers[step[1]].append( towers[step[0]].pop() )
@@ -12,27 +13,25 @@ def move(step, towers):
         return towers
     return []
   
-problem = """ place = [0,1,2,3,4,5,6,7];
+problem = """
+SELECT to([mov.this, tower], "move"+str(level))
+FROM row=[0,1,2,3,4,5,6,7], column=[0,1,2,3,4,5,6,7]
+WHERE checkboard == put(row.this, column.this, checkboard )
               
-              SELECT to([mov.this, tower], "move"+str(level))
-              FROM row=place, column=place
-              WHERE allcheckboard == put(row.this, column.this, checkboard )
-              
-              START WITH level = 1, checkboard = [[0,0,0,0,0,0,0,0],
-                                                  [0,0,0,0,0,0,0,0],
-                                                  [0,0,0,0,0,0,0,0],
-                                                  [0,0,0,0,0,0,0,0],
-                                                  [0,0,0,0,0,0,0,0],
-                                                  [0,0,0,0,0,0,0,0],
-                                                  [0,0,0,0,0,0,0,0],
-                                                  [0,0,0,0,0,0,0,0]]
-              CONNECT BY checkboard = put( row.this, column.this, checkboard ), level=level+1
-              STOP WITH level == 8
-              
-              AS list;
-              """
+START WITH level = 1, checkboard = [[0,0,0,0,0,0,0,0],
+                                    [0,0,0,0,0,0,0,0],
+                                    [0,0,0,0,0,0,0,0],
+                                    [0,0,0,0,0,0,0,0],
+                                    [0,0,0,0,0,0,0,0],
+                                    [0,0,0,0,0,0,0,0],
+                                    [0,0,0,0,0,0,0,0],
+                                    [0,0,0,0,0,0,0,0]]
+CONNECT BY checkboard = place(row.this, column.this, checkboard ), level=level+1
+STOP WITH level == 8 or checkerboard == []
 
-bytecode = SelectScript.compile(problem, debug=True)
+AS list; """
+
+bytecode = SelectScript.compile(problem)
 
 ss = SelectScript.Interpreter()
 ss.addFunction('move', move)
