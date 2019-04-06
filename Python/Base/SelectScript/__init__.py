02b8369fdafbb361e972d7dfbdf90502825cc9d9
André Dietrich <andre.dietrich@ovgu.de>
2019-04-06 12:52:27 +0200

    updated project

--- Python/Base/SelectScript/__init__.py
+++
@@ -1,8 +0,0 @@
-from Interpreter import Interpreter
-from SelectScript import SelectScript
-
-def compile(prog, debug=True):
-    try:
-        return SelectScript(None, None).compile(prog, debug)
-    except:
-        print "Error"
